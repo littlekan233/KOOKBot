cad540b5132348ab661261f3a338f5021b5c2d79
一个小阚阚 <little-kan@qq.com>
2023-07-08 10:07:36 +0800

    Initial commit

---
+++ src/main/java/ml/littlekan/kookbot/KOOKBot.java
@@ -0,0 +1,17 @@
+package ml.littlekan.kookbot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KOOKBot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
